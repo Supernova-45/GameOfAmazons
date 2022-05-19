1558d905421b50fc3c3dcf4350fdf5c9ddfc293c
Alexandra Kim <alexandrasuriya@gmail.com>
2022-05-18 18:26:01 -0700

    Added initial files

---
+++ Amazons.java
@@ -0,0 +1,27 @@
+public class Amazons {
+    public static void main(String[] args) {
+        int[][] board;
+
+    }
+
+    public static String printBoard() {
+        return "";
+    }
+
+    public static int[][] initialBoard() {
+
+    }
+
+    public static void play() {
+        // might not need
+    }
+
+    public static boolean checkDone() {
+        return true;
+    }
+
+    public static String rules() {
+        return "";
+    }
+
+}
