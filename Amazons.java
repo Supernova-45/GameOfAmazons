82d7eaa2fcf0c92c26322aa426d3d250633d1fef
Alexandra Kim <alexandrasuriya@gmail.com>
2022-05-19 10:44:53 -0700

    Updated printBoard

--- Amazons.java
+++ Amazons.java
@@ -1,15 +1,34 @@
 public class Amazons {
     public static void main(String[] args) {
-        int[][] board;
+        int[][] board = new int[10][10];
 
     }
 
-    public static String printBoard() {
-        return "";
+    // formats the board so that it's readable
+    public static String printBoard(int[][] board) {
+        String printBoard = "";
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                if (board[r][c] == 0) {
+                    printBoard += ". ";
+                } else {
+                    printBoard += board[r][c] + " ";
+                }
+            }
+            printBoard += "\n";
+        }
+        return printBoard;
     }
 
-    public static int[][] initialBoard() {
-
+    // formats the starting board
+    public static int[][] initialBoard(int size) {
+        int[][] board = new int[size][size];
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                board[r][c] = 0;
+            }
+        }
+        return board;
     }
 
     public static void play() {
