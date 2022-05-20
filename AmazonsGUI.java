519436c5eb9c9912ecf27a9914c51d6c2bc665d0
Alexandra Kim <alexandrasuriya@gmail.com>
2022-05-20 09:44:34 -0700

    Fixed GUI file spacing

--- AmazonsGUI.java
+++ AmazonsGUI.java
@@ -2,15 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
-
-   class AmazonsGUI{
-      public static void main(String args[]){
+public class AmazonsGUI {
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Game of the Amazons");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
-       JButton button1 = new JButton("Press");
-       frame.getContentPane().add(button1);
-       frame.setVisible(true);
-     }
+        JButton button1 = new JButton("Press");
+        frame.getContentPane().add(button1);
+        frame.setVisible(true);
+    }
 }
+
