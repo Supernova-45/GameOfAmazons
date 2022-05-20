f8e8142c77c06ccab360776f84a687f2faee7307
Alexandra Kim <alexandrasuriya@gmail.com>
2022-05-20 12:23:03 -0700

    Added button grid GUI

--- AmazonsGUI.java
+++ AmazonsGUI.java
@@ -2,14 +2,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class AmazonsGUI {
+public class AmazonsGUI extends JPanel {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Game of the Amazons");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        frame.setSize(300, 300);
+
+        JPanel panel = new JPanel();    
+        frame.add(panel);
+
+
         JButton button1 = new JButton("Press");
         frame.getContentPane().add(button1);
         frame.setVisible(true);
+
+        //grid();
     }
+    
+    public static void grid() {
+        JButton[] buttons = new JButton[25];
+		//setLayout(new GridLayout(3,3));
+		for(int i = 0; i < 25; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setText("-");
+            buttons[i].setBackground(Color.white);
+            //buttons[i].addActionListener(new ActionListener()) { }
+            //add(buttons[i]); //adds this button to JPanel        
+        }	
+	}
 }
 
---
+++ ButtonGrid.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ButtonGrid {
+
+	JFrame frame=new JFrame(); //creates frame
+	JButton[][] grid; //names the grid of buttons
+
+    public ButtonGrid(int[][] board) { //constructor
+		frame.setLayout(new GridLayout(board.length,board[0].length)); //set layout
+		grid=new JButton[board.length][board[0].length]; //allocate the size of grid
+		for(int row=0; row<board.length; row++){
+			for(int col=0; col<board[0].length; col++){
+				grid[row][col]=new JButton(Integer.toString(board[row][col])); //creates new button	  
+				frame.add(grid[row][col]); //adds button to grid
+			}
+		}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack(); //sets appropriate size for frame
+		frame.setVisible(true); //makes frame visible
+	}
+
+    public static void main(String[] args) {
+        int[][] board = initialBoard(5);
+		new ButtonGrid(board);//makes new ButtonGrid with 2 parameters
+	}
+
+    public static int[][] initialBoard(int size) {
+        int[][] board = new int[size][size];
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                board[r][c] = 0;
+            }
+        }
+        if (size == 10) {
+            // black queens
+            board[0][3] = 1;
+            board[0][6] = 1;
+            board[3][0] = 1;
+            board[3][9] = 1;
+            // white queens
+            board[6][0] = 2;
+            board[6][9] = 2;
+            board[9][3] = 2;
+            board[9][6] = 2;
+        } else if (size == 8) {
+            // black queens
+            board[0][3] = 1;
+            board[1][7] = 1;
+            board[2][0] = 1;
+            // white queens
+            board[5][7] = 2;
+            board[6][0] = 2;
+            board[7][4] = 2;
+        } else if (size == 6) {
+            // black queens
+            board[0][3] = 1;
+            board[5][2] = 1;
+            // white queens
+            board[2][0] = 2;
+            board[3][5] = 2;
+        } else if (size == 5) {
+            // black queens
+            board[0][1] = 1;
+            board[0][3] = 1;
+            board[1][0] = 1;
+            board[1][4] = 1;
+            // white queens
+            board[3][0] = 2;
+            board[3][4] = 2;
+            board[4][1] = 2;
+            board[4][3] = 2;
+        } else {
+            System.out.println("Not a valid size, defaulting to 10x10");
+            // black queens
+            board[0][3] = 1;
+            board[0][6] = 1;
+            board[3][0] = 1;
+            board[3][9] = 1;
+            // white queens
+            board[6][0] = 2;
+            board[6][9] = 2;
+            board[9][3] = 2;
+            board[9][6] = 2;
+        }
+        return board;
+    }
+}
